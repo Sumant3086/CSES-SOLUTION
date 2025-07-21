f49441ec99c809acd5a93690e721b73114dce25b
sumant3086 <sumantyadav3086@gmail.com>
2025-07-21 14:00:28 +0530

    easy with temp var

---
+++ RemovingDigits.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>dp(n+1,INT_MAX);
+    dp[0]=0;
+    for(int i=1;i<=n;i++){
+        int temp=i;
+        while(temp>0){
+            int digit=temp%10;
+            temp/=10;
+            if(digit!=0){
+                dp[i]=min(dp[i],dp[i-digit]+1);
+            }
+        }
+    }
+    cout<<dp[n]<<endl;
+    return 0;
+}
