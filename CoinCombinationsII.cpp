83e4e066be4fa3f0d5ef4dd16bba4d234cdb0c41
sumant3086 <sumantyadav3086@gmail.com>
2025-07-21 13:54:01 +0530

    one is for permutation and one is for distinct element

---
+++ CoinCombinationsII.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,x;
+    cin>>n>>x;
+    vector<int>coins(n);
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    } 
+    int mod=1e9+7;
+    vector<int>dp(x+1,0);
+    dp[0]=1;
+    for(int c:coins){
+        for(int i=1;i<=x;i++){
+            if(i-c>=0){
+                dp[i]=(dp[i]+dp[i-c])%mod;
+            }
+        }
+    }
+    cout<<dp[x]<<endl;
+}
